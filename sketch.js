9f976bc39dfee0e42c10823514894f286d3d1847
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-02-01 12:02:21 +0200

    Got the rgb values for each pixel

--- sketch.js
+++ sketch.js
@@ -11,5 +11,15 @@ function setup() {
 function draw() {
   background(220);
   image(img, 0, 0, width, height)
-  
+
+  for (let i = 0; i < img.height; i++) {
+    for (let j = 0; j < img.width; j++) {
+      const pixelIndex = (i + j * img.width) * 4
+
+      const r = img.pixels[pixelIndex + 0]
+      const g = img.pixels[pixelIndex + 1]
+      const b = img.pixels[pixelIndex + 2]
+    }
+    
+  }
 }
