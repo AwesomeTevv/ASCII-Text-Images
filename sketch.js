538ab9b0f1a6ebefac837be0a69612f4a7f61813
AwesomeTevv <tevlen.naidoo@gmail.com>
2023-02-02 16:32:54 +0200

    Fixed the issue where the image would be flipped

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const density = "Ñ@#W$9876543210?!abc;:+=-,._               ";
+const density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
 
 let img;
 
@@ -9,18 +9,12 @@ function preload() {
 function setup() {
   noCanvas();
 
-  background(0);
-  // image(img, 0, 0, width, height);
-
-  let w = width / img.width;
-  let h = height / img.height;
-
   img.loadPixels();
 
-  for (let i = 0; i < img.height; i++) {
-    let row = '';
-    for (let j = 0; j < img.width; j++) {
-      const pixelIndex = (i + j * img.width) * 4;
+  for (let i = 0; i < img.width; i++) {
+    let row = "";
+    for (let j = 0; j < img.height; j++) {
+      const pixelIndex = (j + i * img.width) * 4;
 
       const r = img.pixels[pixelIndex + 0];
       const g = img.pixels[pixelIndex + 1];
@@ -31,7 +25,9 @@ function setup() {
       const len = density.length;
       const charIndex = floor(map(avg, 0, 255, len, 0));
 
-      row += density.charAt(charIndex);
+      const c = density.charAt(charIndex);
+      if (c == " ") row += '&nbsp;';
+      else row += c;
     }
     createDiv(row)
   }
